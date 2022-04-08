307628fa57d1088623e792f6686bf679688d8169
sumanth <sumanth.sarvepalli@gmail.com>
2022-04-08 12:03:31 +0530

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/*
+ *
+ * Title        : Bank Management System
+ * Author       : Sumanth
+ * Description  : This projects a Bank Management System
+ *
+ */
+
+#include <iostream> 
+
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to Bank management system" << endl;
+
+    return 0;
+}
